b2c9b5f162940c3bbc4ebf6de9fdc9769e64f3a4
Camden Webster <59626059+camdenwebster@users.noreply.github.com>
2025-08-08 20:32:12 -0500

    Initial Commit

---
+++ GluCoachTests/GluCoachTests.swift
@@ -0,0 +1,16 @@
+//
+//  GluCoachTests.swift
+//  GluCoachTests
+//
+//  Created by Camden Webster on 8/8/25.
+//
+
+import Testing
+
+struct GluCoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
